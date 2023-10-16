2d91af104a3b37e8a6dc4b0b3a96ca9397aaa8d6
Paragongamer773 <ali.rashid24a@gmail.com>
2023-10-16 18:51:28 -0500

    Workout Log: Humble Beginings
    Intial Development of workout log. Trying to figure out class structure and user workflow through the workout tracker screen

---
+++ Assets/Scripts/Excersise.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Excerises : MonoBehaviour
+{
+    public string exceriseName;
+    public List<int> reps;
+
+}
